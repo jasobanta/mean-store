16871722eacb27239499c7f59813bec4e9c46719
sirs <j100775@gmail.com>
2017-12-18 23:55:53 +0530

    Commited after category and product controll.

--- client/app/category/products/product.controller.js
+++ client/app/category/products/product.controller.js
@@ -1,27 +1,24 @@
   'use strict';
 
-export default class CategoryController {
+export default class ProductController {
   products: Object[];
-  catename: String;
+  purl: String;
   catInfo: Object[];
   $http;
   $scope;
   socket;
+  $window;
+
   /*@ngInject*/
   constructor($http, $scope, socket, $stateParams) {
     this.$http = $http;
     this.$scope = $scope;
     this.socket = socket;
-    this.catename = $stateParams.catename;
+    this.purl = $stateParams.purl;
   }
   $onInit() {
     //this.$http.
     // get all products of the categrory
-    this.$http.get('/api/products')
-      .then(response => {
-        this.products = response.data;
-        console.log(this.products);
-//        this.socket.syncUpdates('Product', this.products);
-      });
+    console.log(this.purl);
   }
 }
