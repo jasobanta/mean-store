be3e0a0194917222f9353e977b14d8b5ad06e235
jasobanta <j100775@gmail.com>
2018-01-02 00:59:47 +0530

    orderlisting

--- client/app/dashboard/index.js
+++ client/app/dashboard/index.js
@@ -4,9 +4,10 @@ import angular from 'angular';
 import routes from './dashboard.routes';
 import catlist from './catlist';
 import ms from './masters';
+import ord from './orders';
 import DashboardController from './dashboard.controller';
 
-export default angular.module('dorbbyfullstackApp.dashboard', ['dorbbyfullstackApp.auth', 'ui.router', catlist, ms])
+export default angular.module('dorbbyfullstackApp.dashboard', ['dorbbyfullstackApp.auth', 'ui.router', catlist, ms, ord])
 .config(routes)
 .controller('DashboardController', DashboardController)
 .name;
---
+++ client/app/dashboard/orders/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+import angular from 'angular';
+import OrderController from './order.controller';
+
+export default angular.module('dorbbyfullstackApp.ord',[])
+.controller('OrderController',OrderController)
+.name;
