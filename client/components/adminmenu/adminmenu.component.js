e01970882e2438e2e6396b0f5ac176b71d4545d6
sirs <j100775@gmail.com>
2018-01-03 20:52:54 +0530

    admin side bar menu now dynamic

--- client/components/adminmenu/adminmenu.component.js
+++ client/components/adminmenu/adminmenu.component.js
@@ -5,15 +5,47 @@ import angular from 'angular';
 
 export class AdminMenuComponent {
   menu = [{
-    title: 'Home',
-    state: 'main'
-  }];
+    title: 'Admin Settings',
+    state: 'admin',
+    class: 'fa fa-cog',
+    submenu: [
+      {title: 'Add Masters', state: 'mastersetting-add'},
+      {title: 'Master Configuration List', state: 'mastersetting'}
+  ]
+},
+{
+  title: 'Category Management',
+  state: 'category',
+  class: 'fa fa-folder',
+  submenu: [
+    {title: 'Add New Main Categories', state: 'addrootcat'},
+    {title: 'List All Main Categories', state: 'rootcatlist'},
+    {title: 'Add New Sub Categories', state: 'addsubcategory'},
+    {title: 'List All Sub Categories', state: 'subcategorylist'},
+    {title: 'Add New Item Categories', state: 'additemcategory'},
+    {title: 'List All Item Categories', state: 'itemcategorylist'},
+    {title: 'Add New Item Sub Categories', state: 'additemsubcategory'},
+    {title: 'List All Item Sub Categories', state: 'itemsubcategorylist'},
+    {title: 'Add New Type Categories', state: 'addtypecategory'},
+    {title: 'List All Type Categories', state: 'typecategorylist'}
+]
+},
+{
+  title: 'Order Management',
+  state: 'order',
+  class: 'fa fa-file',
+  submenu: [
+    {title: 'List of Orders', state: 'orderslist'},
+]
+}
+];
   isLoggedIn: Function;
   isAdmin: Function;
   getCurrentUser: Function;
   isCollapsed = true;
   $state;
   $currentstate;
+  menustate = '';
 
   constructor(Auth, $state) {
     'ngInject';
@@ -26,12 +58,27 @@ export class AdminMenuComponent {
 
   }
   $onInit(){
+    var currentstate = this.currentstate;
+    var menustate = '';
     if(['addrootcat', 'addrootcatlist', 'addsubcategory', 'subcategorylist',
      'itemcategorylist', 'additemcategory', 'additemsubcategory', 'itemsubcategorylist',
-      'rootcatlist', 'addtypecategory', 'typecategorylist'].indexOf(this.currentstate)!==-1) {
+      'rootcatlist', 'addtypecategory', 'typecategorylist', 'mastersetting', 'mastersetting-add'].indexOf(this.currentstate)!==-1) {
       this.isCollapsed = false;
     }
-//console.log(this.isCollapsed);
+    angular.forEach(this.menu,function(values,keys){
+      var submenu = values.submenu;
+      var valuestate = values.state;
+      console.log('currentstate'+currentstate);
+      console.log(values);
+      angular.forEach(submenu, function(value, key){
+        console.log('currentstate'+currentstate);
+        console.log(value);
+        if (currentstate === value.state) {
+          menustate = values.state;
+        }
+      });
+    });
+    this.menustate = menustate;
   }
 
 }
