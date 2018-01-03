42352d9387c81ff41c1d2e0ddf809004616bb34e
jasobanta <j100775@gmail.com>
2018-01-03 14:59:00 +0530

    Add new button in all listing page

--- client/app/dashboard/catlist/catlist.controller.js
+++ client/app/dashboard/catlist/catlist.controller.js
@@ -49,10 +49,12 @@ export default class CatlistController {
       .then(response => {
         this.category = response.data;
         this.rootcat = response.data;
+
       });
     }else{
       //
       this.rootcat = {name: '', isparent: true, sort: '', active: false};
+      
     }
   }
   createCategory(form){
